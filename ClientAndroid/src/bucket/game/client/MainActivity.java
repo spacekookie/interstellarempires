6735d2f1dd50d9b16102ef9958e8a34fc16ffd69
Katharina Fey <kookie@spacekookie.de>
2012-12-17 00:31:50 +0100

    Created LibGDX Client Project + Android/ Desktop and HTML compile projects. Also created first basic class structure for the game. Did not start working with TWL yet because it turns out…there are no documentations for that thing...
    Former-commit-id: 7720f67a9cf4f7417d703448f50d1fc9a1f2bc37

---
+++ ClientAndroid/src/bucket/game/client/MainActivity.java
@@ -0,0 +1,19 @@
+package bucket.game.client;
+
+import android.os.Bundle;
+import bucket.game.client.core.MainClientLauncher;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class MainActivity extends AndroidApplication {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+        cfg.useGL20 = false;
+        
+        initialize(new MainClientLauncher(), cfg);
+    }
+}
