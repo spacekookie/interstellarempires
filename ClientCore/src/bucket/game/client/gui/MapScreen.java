0f6c13a50488e865dd21c5d47d79446a361da1ca
Katharina Fey <kookie@spacekookie.de>
2012-12-17 00:31:50 +0100

    Created LibGDX Client Project + Android/ Desktop and HTML compile projects. Also created first basic class structure for the game. Did not start working with TWL yet because it turns out…there are no documentations for that thing...
    Former-commit-id: cf0f97f9f11f0f1863624b1c60dba9f19e910b31

---
+++ ClientCore/src/bucket/game/client/gui/MapScreen.java
@@ -0,0 +1,70 @@
+package bucket.game.client.gui;
+
+/* 
+ * Copyright (c) 2012 ***REMOVED***
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * 
+ * @author: ***REMOVED***
+ */
+public class MapScreen implements Screen {
+
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
