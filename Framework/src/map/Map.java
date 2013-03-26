7f93e92b0879ba0f277b7cd802ab2d1a6d970edf
Leander Sabel <leander.sabel@gmail.com>
2013-03-26 13:12:44 +0100

    Issue #10 - QuadTree
    - Added the basic structure of the tree
    - Added some other required data structures
    - Added the tree to the map and implemented a first query function
    
    
    Former-commit-id: a7c06c8e1dd024bc23f609dbeca5269efe3cd6be

--- Framework/src/map/Map.java
+++ Framework/src/map/Map.java
@@ -17,8 +17,33 @@
 
 package map;
 
+import java.util.Set;
+
+import objects.GameObject;
+import datastructures.Interval;
+import datastructures.Interval2D;
+import datastructures.QuadTree;
+
 public class Map {
 
-  private SolarSystem[][] solarSystems;
-  
+  private QuadTree<Double, GameObject> objects;
+
+  public Map() {
+	objects = new QuadTree<Double, GameObject>();
+  }
+
+  /**
+   * Get all game objects from a given position and range. This will query the map for game objects
+   * from Position - range to Position + range.
+   * 
+   * @param pos
+   * @param radius
+   * @return
+   */
+  public Set<GameObject> getObjects(Location pos, Double range) {
+	Interval<Double> xIntervall = new Interval<Double>(pos.getX() - range, pos.getX() + range);
+	Interval<Double> yIntervall = new Interval<Double>(pos.getY() - range, pos.getY() + range);
+	Interval2D<Double> intervall = new Interval2D<Double>(xIntervall, yIntervall);
+	return objects.query2D(intervall);
+  }
 }
