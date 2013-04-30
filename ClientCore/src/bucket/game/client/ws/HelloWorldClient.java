5161c52cb85c2fdbb23594e832560c98e3cb6959
Leander Sabel <leander.sabel@gmail.com>
2013-04-30 16:32:56 +0200

    Issue #4
    - Moved server runtime
    - Created sample client
    - Created sample service
    - Updated .classpath files
    
    
    Former-commit-id: 26624c2b5fddf07ff2430f04276f675b590de686

---
+++ ClientCore/src/bucket/game/client/ws/HelloWorldClient.java
@@ -0,0 +1,58 @@
+/* 
+ * Copyright (c) 2013 Leander Sabel
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package bucket.game.client.ws;
+
+
+import interfaces.HelloWorld;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class HelloWorldClient implements HelloWorld {
+  private HelloWorld helloWorldService;
+
+  /**
+   * Default constructor
+   * 
+   * @param url
+   *          The URL to the Hello World WSDL endpoint.
+   */
+  public HelloWorldClient(final URL wsdlUrl) {
+	QName serviceName = new QName("http://webservices.samples.jboss.org/", "HelloWorldImplService");
+
+	Service service = Service.create(wsdlUrl, serviceName);
+	helloWorldService = service.getPort(HelloWorld.class);
+	assert (helloWorldService != null);
+	
+	System.out.println(helloWorldService.sayHello("Bob"));
+  }
+
+  @Override
+  public String sayHello(String name) {
+	// TODO Auto-generated method stub
+	return null;
+  }
+  
+  public static void main(String[] args) throws MalformedURLException {
+	HelloWorldClient client = new HelloWorldClient(new URL("http://localhost:8080/Server-WS/HelloWorld?wsdl"));
+  }
+
+}
