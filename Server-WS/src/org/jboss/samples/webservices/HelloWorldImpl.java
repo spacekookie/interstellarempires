928c07ad43392b0b68269199a062f58629dc43cc
Leander Sabel <leander.sabel@gmail.com>
2013-05-01 17:28:48 +0200

    Issue #4
    - Created new POJO server project
    - Refactored some things in the serverWS project
    - Added the correct references to the JBoss config
    - Created a main ServerWS "loop"
    
    
    Former-commit-id: eac4a8e8d34de1d3e0d7e6419403fea5cd5b74c4

--- Server-WS/src/org/jboss/samples/webservices/HelloWorldImpl.java
+++ Server-WS/src/org/jboss/samples/webservices/HelloWorldImpl.java
@@ -1,17 +1,27 @@
 package org.jboss.samples.webservices;
 
 
+import javax.ejb.EJB;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
+import server.ws.core.ServerWS;
 import server.ws.interfaces.HelloWorld;
 
 @WebService()
 public class HelloWorldImpl implements HelloWorld {
 
+//  @EJB
+//  ServerWS server;
+  
   @WebMethod()
   public String sayHello(String name) {
       System.out.println("Hello: " + name);
+      
+//      server.test();
+
       return "Hello " + name + "!";
+     
+      
   }
 }
