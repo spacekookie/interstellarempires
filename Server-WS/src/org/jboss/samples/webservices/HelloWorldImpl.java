dd029d40107cea74a6e92f772f71913e6b3420e7
Leander Sabel <leander.sabel@gmail.com>
2013-05-02 14:27:54 +0200

    Issue #12
    - Added session management
    
    
    Former-commit-id: 32080d1256a48676089cc7b160fc922d8778e432

--- Server-WS/src/org/jboss/samples/webservices/HelloWorldImpl.java
+++ Server-WS/src/org/jboss/samples/webservices/HelloWorldImpl.java
@@ -1,27 +1,17 @@
 package org.jboss.samples.webservices;
 
-
-import javax.ejb.EJB;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
-import server.ws.core.ServerWS;
 import server.ws.interfaces.HelloWorld;
 
 @WebService()
 public class HelloWorldImpl implements HelloWorld {
 
-//  @EJB
-//  ServerWS server;
-  
   @WebMethod()
   public String sayHello(String name) {
-      System.out.println("Hello: " + name);
-      
-//      server.test();
 
-      return "Hello " + name + "!";
-     
-      
+	return "Hello " + name + "!";
+
   }
 }
