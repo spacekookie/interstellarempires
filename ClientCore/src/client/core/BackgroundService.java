cfbbec2c116d2087a7b175d85f11a3dada438e3a
Katharina Fey <kookie@spacekookie.de>
2013-05-05 13:27:15 +0200

    Stuff
    Former-commit-id: b8ca9432e377acaafb2ed30e825439a846e59c71

--- ClientCore/src/client/core/BackgroundService.java
+++ ClientCore/src/client/core/BackgroundService.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright (c) 2012 Leander Sabel
+ * Copyright (c) 2013 ***REMOVED***
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,23 +15,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package framework.players;
+package client.core;
 
-public class Player {
-
-	private boolean admin;
-	private String name;
-
-	public Player(String name) {
-		this.name = name;
-	}
-
-	public void setPony(boolean admin) {
-		this.admin = admin;
-	}
-
-	public boolean isAdmin() {
-		return admin;
-	}
+public class BackgroundService extends Thread {
 
 }
