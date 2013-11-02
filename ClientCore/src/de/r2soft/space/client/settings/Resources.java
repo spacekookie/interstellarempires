493a6dcc6b4f3c1984a6e6ebe67cd332747e9c95
Katharina Fey <kookie@spacekookie.de>
2013-11-02 03:33:39 +0100

    Adding Resources back again
    Former-commit-id: 1040d5ff9badf13a12a38f764d8b25120803080b

--- ClientCore/src/de/r2soft/space/client/settings/Resources.java
+++ ClientCore/src/de/r2soft/space/client/settings/Resources.java
@@ -17,7 +17,12 @@
  ######################################################################### */
 package de.r2soft.space.client.settings;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector2;
+import com.sun.tools.javac.util.Pair;
+
 import de.r2soft.space.framework.players.Player;
+import de.r2soft.space.framework.primitives.IntVec2;
 
 /**
  * Initializes the settings for the game. Also responsible for setting new
@@ -30,6 +35,8 @@ public class Resources {
 	/** Just going with 720p game resolution here */
 	public static int OLD_WIDTH = 1280;
 	public static int OLD_HEIGHT = 720;
+	public static int NEW_WIDTH = OLD_WIDTH;
+	public static int NEW_HEIGHT = OLD_HEIGHT;
 	public static String SUPERTITLE = "Game Client";
 	public static String VERSION_NUMBER = "Prototype 1.2";
 	public static final String SCREENTITLE_SETTINGS = "SETTINGS";
@@ -62,5 +69,18 @@ public class Resources {
 	public static final float HEX_RADIUS = 64f;
 	public static final float HEX_COORD_X = (1 / 2) * 64f;
 	public static final float HEX_COORD_Y = (float) ((Math.sqrt(3) * 64f) / 2);
+
+	/** Map sizes (for Hexmap and Solar Map) */
+	private static final IntVec2 mapSize = new IntVec2(920, 520);
+
+	/** Screw around on that to position the map! */
+	private static final IntVec2 mapOffset = new IntVec2(0, -250);
+	/**
+	 * First vector holds starting position of map (lower right corner), the
+	 * other holds the actual size
+	 */
+	public static final Pair<IntVec2, IntVec2> mapDim = new Pair<IntVec2, IntVec2>(
+			new IntVec2(NEW_WIDTH - mapSize.x - mapOffset.x, NEW_HEIGHT
+					- mapSize.y - mapOffset.y), mapSize);
 
 }
