c1941bb82560cf591c4342787143179bc2608e57
Katharina Fey <kookie@spacekookie.de>
2013-11-28 11:13:58 +0100

    Adding new resources, moving stuff, preparing for the big library update to 0.9.9 (STABLE)
    Former-commit-id: 250b3a8eae7d6bea5f78f8432a83c5b72c6d6fd1

---
+++ ClientCore/src/de/r2soft/space/client/types/SolSystemRenderer.java
@@ -0,0 +1,61 @@
+/* #########################################################################
+ * Copyright (c) 2013 Random Robot Softworks
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ ######################################################################### */
+
+package de.r2soft.space.client.types;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.MapRenderer;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.utils.Disposable;
+
+/**
+ * A custom renderer that renders a solar system with a star in the middle and
+ * planets orbiting on circular planes around the star.
+ * 
+ * @author ***REMOVED***
+ * 
+ */
+public class SolSystemRenderer implements MapRenderer, Disposable {
+
+	@Override
+	public void dispose() {
+
+	}
+
+	@Override
+	public void setView(OrthographicCamera camera) {
+
+	}
+
+	@Override
+	public void setView(Matrix4 projectionMatrix, float viewboundsX,
+			float viewboundsY, float viewboundsWidth, float viewboundsHeight) {
+
+	}
+
+	@Override
+	public void render() {
+
+	}
+
+	@Override
+	public void render(int[] layers) {
+
+	}
+
+}
