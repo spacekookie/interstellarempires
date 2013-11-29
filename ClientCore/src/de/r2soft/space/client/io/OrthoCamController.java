c82ad2145bb4ec50c92da0f2fb2b806a16c1b383
Katharina Fey <kookie@spacekookie.de>
2013-11-29 22:11:22 +0100

    Changes in client
    Former-commit-id: 6570cf9e85a9b77f4f3899738e5a89912da10dc8

--- ClientCore/src/de/r2soft/space/client/io/OrthoCamController.java
+++ ClientCore/src/de/r2soft/space/client/io/OrthoCamController.java
@@ -2,53 +2,75 @@ package de.r2soft.space.client.io;
 
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.math.Vector3;
 
+import de.r2soft.space.client.screens.gameplay.HexMapScreen;
+
 public class OrthoCamController extends InputAdapter {
-	final OrthographicCamera camera;
-	final Vector3 curr = new Vector3();
-	final Vector3 last = new Vector3(-1, -1, -1);
-	final Vector3 delta = new Vector3();
+  final OrthographicCamera camera;
+  final Vector3 curr = new Vector3();
+  final Vector3 last = new Vector3(-1, -1, -1);
+  final Vector3 delta = new Vector3();
 
-	public OrthoCamController(OrthographicCamera camera) {
-		this.camera = camera;
+  private TiledMap map;
+
+  public OrthoCamController(OrthographicCamera camera, TiledMap map) {
+	this.camera = camera;
+	this.map = map;
+  }
+
+  @Override
+  public boolean mouseMoved(int screenX, int screenY) {
+	return false;
+  }
+
+  @Override
+  public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+	Vector3 tmp = new Vector3(screenX, screenY, 0);
+	camera.unproject(tmp);
+
+	System.out.println("X:" + tmp.x + " Y: " + tmp.y);
+
+	return false;
+	// Tile t = level.getTileMap().getTileAt(tmp.x, tmp.y);
+  }
+
+  @Override
+  public boolean touchDragged(int x, int y, int pointer) {
+	camera.unproject(curr.set(x, y, 0));
+	if (!(last.x == -1 && last.y == -1 && last.z == -1)) {
+	  camera.unproject(delta.set(last.x, last.y, 0));
+	  delta.sub(curr);
+	  camera.position.add(delta.x, delta.y, 0);
 	}
+	last.set(x, y, 0);
+	return false;
+  }
 
-	@Override
-	public boolean touchDragged(int x, int y, int pointer) {
-		camera.unproject(curr.set(x, y, 0));
-		if (!(last.x == -1 && last.y == -1 && last.z == -1)) {
-			camera.unproject(delta.set(last.x, last.y, 0));
-			delta.sub(curr);
-			camera.position.add(delta.x, delta.y, 0);
-		}
-		last.set(x, y, 0);
-		return false;
-	}
+  public void changeZoom(float zoom, float x, float y) {
 
-	public void changeZoom(float zoom, float x, float y) {
+	Vector3 before = new Vector3(x, y, 0);
+	camera.unproject(before);
 
-		Vector3 before = new Vector3(x, y, 0);
-		camera.unproject(before);
+	camera.zoom = zoom;
+	camera.update();
+	Vector3 after = new Vector3(x, y, 0);
+	camera.unproject(after);
 
-		camera.zoom = zoom;
-		camera.update();
-		Vector3 after = new Vector3(x, y, 0);
-		camera.unproject(after);
+	camera.translate(before.x - after.x, before.y - after.y, 0);
+  }
 
-		camera.translate(before.x - after.x, before.y - after.y, 0);
-	}
+  @Override
+  public boolean scrolled(int amount) {
+	float newZoom = camera.zoom * (1 + (amount < 0 ? 0.1f : -0.1f));
+	changeZoom(newZoom, last.x, last.y);
+	return true;
+  }
 
-	@Override
-	public boolean scrolled(int amount) {
-		float newZoom = camera.zoom * (1 + (amount < 0 ? 0.1f : -0.1f));
-		changeZoom(newZoom, last.x, last.y);
-		return true;
-	}
-
-	@Override
-	public boolean touchUp(int x, int y, int pointer, int button) {
-		last.set(-1, -1, -1);
-		return false;
-	}
+  @Override
+  public boolean touchUp(int x, int y, int pointer, int button) {
+	last.set(-1, -1, -1);
+	return false;
+  }
 }
