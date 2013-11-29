b7462e4102732292d93a4f6381b26a40320df006
Leander Sabel <leander.sabel@gmail.com>
2013-11-29 12:34:12 +0100

    - ServerWS now using wildly - Removed bad ws implementations
    Former-commit-id: fc5a72534b8169a5a64be4b87160fa97560cc2b2

--- ClientCore/src/de/r2soft/space/client/ws/WebServiceClient.java
+++ ClientCore/src/de/r2soft/space/client/ws/WebServiceClient.java
@@ -19,151 +19,79 @@ package de.r2soft.space.client.ws;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Set;
-
 import javax.jws.WebMethod;
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 
 import org.apache.log4j.Logger;
 
-import de.r2soft.space.framework.map.Map;
-import de.r2soft.space.framework.map.SolarSystem;
-import de.r2soft.space.framework.objects.GameObject;
-import de.r2soft.space.framework.objects.PlayerObject;
-import de.r2soft.space.framework.players.Player;
-import de.r2soft.space.framework.types.IntVec2;
 import de.r2soft.space.server.ws.interfaces.ConnectionService;
-import de.r2soft.space.server.ws.interfaces.GameObjectService;
-import de.r2soft.space.server.ws.interfaces.MapService;
 
-public class WebServiceClient implements ConnectionService, GameObjectService,
-		MapService {
+public class WebServiceClient implements ConnectionService {
+  private static WebServiceClient uniqInstance;
 
-	private static WebServiceClient uniqInstance;
+  private static String WSDL_BASE_URL = "http://localhost:8080/ServerWS/";
+  private final Logger log = Logger.getLogger(this.getClass());
 
-	private static String WSDL_BASE_URL = "http://localhost:8080/ServerWS/";
-	private final Logger log = Logger.getLogger(this.getClass());
+  private ConnectionService connectionClient;
 
-	private ConnectionService connectionClient;
-	private GameObjectService gameObjectClient;
+  private Integer sessionID;
 
-	private Integer sessionID;
+  private WebServiceClient() {
+	// Initialize variables
+	sessionID = -1;
 
-	private WebServiceClient() {
-		// Initialize variables
-		sessionID = -1;
+	try {
+	  QName connectionServiceName = new QName("http://2rSoftworks.de/", "ConnectionService");
 
-		try {
-			QName connectionServiceName = new QName("http://2rSoftworks.de/",
-					"ConnectionService");
-			QName gameObjectServiceName = new QName("http://2rSoftworks.de/",
-					"GameObjectService");
+	  URL connectionServiceWsdl = new URL(WSDL_BASE_URL + connectionServiceName.getLocalPart()
+		  + "?wsdl");
 
-			URL connectionServiceWsdl = new URL(WSDL_BASE_URL
-					+ connectionServiceName.getLocalPart() + "?wsdl");
-			URL gameObjectServiceWsdl = new URL(WSDL_BASE_URL
-					+ gameObjectServiceName.getLocalPart() + "?wsdl");
+	  Service connectionService = Service.create(connectionServiceWsdl, connectionServiceName);
 
-			Service connectionService = Service.create(connectionServiceWsdl,
-					connectionServiceName);
-			Service gameObjectService = Service.create(gameObjectServiceWsdl,
-					gameObjectServiceName);
-
-			connectionClient = connectionService
-					.getPort(ConnectionService.class);
-			gameObjectClient = gameObjectService
-					.getPort(GameObjectService.class);
-		} catch (MalformedURLException e) {
-			log.error("Invalid wsdl URL", e);
-		}
+	  connectionClient = connectionService.getPort(ConnectionService.class);
 	}
-
-	/**
-	 * Get a handle to this singleton WebService client.
-	 * 
-	 * @return
-	 */
-	public static synchronized WebServiceClient getInstance() {
-		if (uniqInstance == null) {
-			uniqInstance = new WebServiceClient();
-		}
-		return uniqInstance;
+	catch (MalformedURLException e) {
+	  log.error("Invalid wsdl URL", e);
 	}
+  }
 
-	/**
-	 * Get the session ID used to request information from the server.
-	 * 
-	 * @return
-	 */
-	public Integer getSessionID() {
-		return sessionID;
+  /**
+   * Get a handle to this singleton WebService client.
+   * 
+   * @return
+   */
+  public static synchronized WebServiceClient getInstance() {
+	if (uniqInstance == null) {
+	  uniqInstance = new WebServiceClient();
 	}
+	return uniqInstance;
+  }
 
-	@Override
-	@WebMethod
-	public Integer connect(String username, String password) {
-		return connectionClient.connect(username, password);
-	}
+  /**
+   * Get the session ID used to request information from the server.
+   * 
+   * @return
+   */
+  public Integer getSessionID() {
+	return sessionID;
+  }
 
-	@Override
-	@WebMethod
-	public boolean disconnect(Integer sessionID) {
-		return connectionClient.disconnect(sessionID);
-	}
+  @Override
+  @WebMethod
+  public Integer connect(String username, String password) {
+	return connectionClient.connect(username, password);
+  }
 
-	@Override
-	@WebMethod
-	public Set<GameObject> getGlobalGameObjects(Integer sessionID) {
-		return gameObjectClient.getGlobalGameObjects(sessionID);
-	}
+  @Override
+  @WebMethod
+  public boolean disconnect(Integer sessionID) {
+	return connectionClient.disconnect(sessionID);
+  }
 
-	@Override
-	@WebMethod
-	public Set<PlayerObject> getPlayerObjects(Integer sessionID) {
-		return gameObjectClient.getPlayerObjects(sessionID);
-	}
-
-	@Override
-	@WebMethod
-	public Set<PlayerObject> getObjectsInSystem(Integer sessionID,
-			IntVec2 system) {
-		return gameObjectClient.getObjectsInSystem(sessionID, system);
-	}
-
-	public static void main(String[] args) {
-		WebServiceClient client = WebServiceClient.getInstance();
-		System.out.println(client.connect("Bob", "*****"));
-	}
-
-	@Override
-	@WebMethod
-	public Set<SolarSystem> getGlobalSolarSystems(Integer sessionID) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	@WebMethod
-	public Set<SolarSystem> getPlaySolarSystems(Integer sessionID, Player player) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	@WebMethod
-	public Set<SolarSystem> getKnownSolarSystems(Integer sessionID,
-			Player player) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	@WebMethod
-	public Map getPlayerViewScreen(IntVec2 bottomLeftSystem, float mapWidth,
-			float mapHeight) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  public static void main(String[] args) {
+	WebServiceClient client = WebServiceClient.getInstance();
+	System.out.println(client.connect("Bob", "*****"));
+  }
 
 }
