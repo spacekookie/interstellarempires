28a333698d376d2e28ca3a63609c98a1d3c9db1a
Katharina Fey <kookie@spacekookie.de>
2013-11-29 13:11:00 +0100

    Cleaning up after the apocalypse
    Former-commit-id: 1efa3c396039511608aa8b3be84b6c83d10d6080

--- ClientCore/src/de/r2soft/space/client/screens/utilities/LoginScreen.java
+++ ClientCore/src/de/r2soft/space/client/screens/utilities/LoginScreen.java
@@ -30,7 +30,6 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
-import de.r2soft.space.client.a.depr.HexagonScreen;
 import de.r2soft.space.client.core.ScreenHandler;
 import de.r2soft.space.client.screens.gameplay.HexMapScreen;
 import de.r2soft.space.client.settings.Resources;
