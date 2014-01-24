d4e0653854c6a1ea55d378f42d5b3600af7b1f4c
Katharina Fey <kookie@spacekookie.de>
2014-01-24 12:18:49 +0100

    Updating Pairs to Java7. (with Keys and Values). Also depricating them. Trying to find better system
    Former-commit-id: 48e61c65773c94611904916533f42325807663f0

--- ClientCore/src/de/r2soft/space/client/settings/BaseSettings.java
+++ ClientCore/src/de/r2soft/space/client/settings/BaseSettings.java
@@ -17,62 +17,60 @@
  ######################################################################### */
 package de.r2soft.space.client.settings;
 
-import com.badlogic.gdx.Gdx;
-import com.sun.tools.javac.util.Pair;
-
 import de.r2soft.space.framework.players.Player;
 import de.r2soft.space.framework.types.IntVec2;
 
 /**
- * Initializes the settings for the game. Also responsible for setting new Settings.
+ * Initializes the settings for the game. Also responsible for setting new
+ * Settings.
  * 
  * @author: ***REMOVED***
  */
 public class BaseSettings {
 
-  /** Just going with 720p game resolution here */
-  public static int OLD_WIDTH = 1280;
-  public static int OLD_HEIGHT = 720;
-  public static int NEW_WIDTH = OLD_WIDTH;
-  public static int NEW_HEIGHT = OLD_HEIGHT;
-  public static String SUPERTITLE = "Game Client";
-  public static String VERSION_NUMBER = "Prototype 1.2";
-  public static final String SCREENTITLE_SETTINGS = "SETTINGS";
-  public static final String SCREENTITLE_HOME = "HOME";
-  public static final String SCREENTITLE_LOGIN = "LOGIN";
-  public static final String SCREENTITLE_SOLAR = "SOL SYSTEM";
+	/** Just going with 720p game resolution here */
+	public static int OLD_WIDTH = 1280;
+	public static int OLD_HEIGHT = 720;
+	public static int NEW_WIDTH = OLD_WIDTH;
+	public static int NEW_HEIGHT = OLD_HEIGHT;
+	public static String SUPERTITLE = "Game Client";
+	public static String VERSION_NUMBER = "Prototype 1.2";
+	public static final String SCREENTITLE_SETTINGS = "SETTINGS";
+	public static final String SCREENTITLE_HOME = "HOME";
+	public static final String SCREENTITLE_LOGIN = "LOGIN";
+	public static final String SCREENTITLE_SOLAR = "SOL SYSTEM";
 
-  /** Log sings */
-  public final static String LOG_GLOBAL = "From Client";
-  public final static String LOG_HEXMAP = "From HexMap";
-  public final static String LOG_HEX_TILE = "From HexMap";
-  public final static String LOG_SOLAR_MAP = "From System";
-  public final static String LOG_MAP_OBJECT = "From Object";
+	/** Log sings */
+	public final static String LOG_GLOBAL = "From Client";
+	public final static String LOG_HEXMAP = "From HexMap";
+	public final static String LOG_HEX_TILE = "From HexMap";
+	public final static String LOG_SOLAR_MAP = "From System";
+	public final static String LOG_MAP_OBJECT = "From Object";
 
-  public static final String PREFERENCE_FILE_NAME = "de.r2.space.client.main-prefereces";
-  public static final String PREFERENCE_SKIP_INTRO = "skip_intro";
-  public static final String PREFERENCE_PLAY_MUSIC = "play_background_music";
-  public static final String PREFERENCE_SAVE_USERNAME = "save_login_user_name";
-  public static final String PREFERENCE_SAVED_USER_NAME = "saved_user_name";
+	public static final String PREFERENCE_FILE_NAME = "de.r2.space.client.main-prefereces";
+	public static final String PREFERENCE_SKIP_INTRO = "skip_intro";
+	public static final String PREFERENCE_PLAY_MUSIC = "play_background_music";
+	public static final String PREFERENCE_SAVE_USERNAME = "save_login_user_name";
+	public static final String PREFERENCE_SAVED_USER_NAME = "saved_user_name";
 
-  /** Map Sub-sizes */
-  public static final IntVec2 mapSize = new IntVec2(900, 600);
+	/** Map Sub-sizes */
+	public static final IntVec2 mapSize = new IntVec2(900, 600);
 
-  @Deprecated
-  /** Needs to be made dynamic and fetched from server */
-  public static final Player thisPlayer = new Player("KateTheAwesome");
-  public static final Player _neutralplayer = new Player("_no_owning_player_");
+	@Deprecated
+	/** Needs to be made dynamic and fetched from server */
+	public static final Player thisPlayer = new Player("KateTheAwesome");
+	public static final Player _neutralplayer = new Player("_no_owning_player_");
 
-  /** Hexmap formulas */
-  @Deprecated
-  public static final float HEX_START_X = -275f;
-  @Deprecated
-  public static final float HEX_START_Y = -100f;
-  @Deprecated
-  public static final float HEX_RADIUS = 64f;
-  @Deprecated
-  public static final float HEX_COORD_X = (1 / 2) * 64f;
-  @Deprecated
-  public static final float HEX_COORD_Y = (float) ((Math.sqrt(3) * 64f) / 2);
+	/** Hexmap formulas */
+	@Deprecated
+	public static final float HEX_START_X = -275f;
+	@Deprecated
+	public static final float HEX_START_Y = -100f;
+	@Deprecated
+	public static final float HEX_RADIUS = 64f;
+	@Deprecated
+	public static final float HEX_COORD_X = (1 / 2) * 64f;
+	@Deprecated
+	public static final float HEX_COORD_Y = (float) ((Math.sqrt(3) * 64f) / 2);
 
 }
