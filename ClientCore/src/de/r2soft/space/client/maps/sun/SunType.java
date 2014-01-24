d22d3f514f0b96e3d15d1741be46e502feb2761d
Katharina Fey <kookie@spacekookie.de>
2014-01-24 12:18:49 +0100

    Updating Pairs to Java7. (with Keys and Values). Also depricating them. Trying to find better system
    Former-commit-id: ce8662a3249e517a4372f1c6ccc3f295fd34b46f

---
+++ ClientCore/src/de/r2soft/space/client/maps/sun/SunType.java
@@ -0,0 +1,32 @@
+/* #########################################################################
+ * Copyright (c) 2014 Random Robot Softworks
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ ######################################################################### */
+
+package de.r2soft.space.client.maps.sun;
+
+import com.badlogic.gdx.math.Vector2;
+
+import de.r2soft.space.framework.objects.GameObject;
+
+public abstract class SunType {
+
+  private GameObject parentBody;
+  private Vector2 orbitalVelocity;
+  private float orbitalAngle;
+  private float radius;
+
+}
