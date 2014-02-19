6f5dc9fb5d579f4f217c45af7d976a9b1e5e8b9c
IGBC <fakespamtrap@gmail.com>
2014-02-19 18:01:58 +0000

    tweaking values so planet serves as an orbit demo
    Former-commit-id: 2b29317bed511015f97c0d25e962a266b2ae45c6

--- RobotPhysics/src/de/r2soft/robotphysics/tests/Body.java
+++ RobotPhysics/src/de/r2soft/robotphysics/tests/Body.java
@@ -51,7 +51,7 @@ public class Body {
 	  sprite = new Sprite(new Texture(Gdx.files.internal("assets/planet.png")));
 	  sprite.setScale(0.5f);
 	  body = new OrbitalBody(R2P.R2_BODY_BIFUNCTION, this);
-	  updatePosition(new R2Float(300, 150));
+	  updatePosition(new R2Float(300, 300));
 	}
 	else if (type.equals(TYPE.STAR)) {
 	  sprite = new Sprite(new Texture(Gdx.files.internal("assets/star.png")));
