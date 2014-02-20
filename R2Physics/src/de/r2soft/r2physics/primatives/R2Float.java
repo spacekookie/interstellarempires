65ff21d0df09c9f7ef080dc5883b6ce34a8222f4
Katharina Fey <kookie@spacekookie.de>
2014-02-20 21:18:05 +0100

    Adding Android demo application Changing Android compatebility Changing Body creation code
    NOTE: TO MAKE THE DESKTOP DEMO RUN ON A COMPUTER CHANGE THE RUN CONFIGURATIONS WORKING DIRECTORY TO THIS: "${workspace_loc:R2Physics-android/assets}"
    
    Former-commit-id: 78a04b671598763231810e40abdae455c6e5c4c3

---
+++ R2Physics/src/de/r2soft/r2physics/primatives/R2Float.java
@@ -0,0 +1,42 @@
+/* #########################################################################
+ * Copyright (c) 2014 Random Robot Softworks
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ ######################################################################### */
+
+package de.r2soft.r2physics.primatives;
+
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * TODO: FIX ALL OF THIS NONSENSE!
+ * */
+@Deprecated
+public class R2Float extends Vector2 {
+
+  public R2Float() {
+	super();
+  }
+
+  public R2Float(float f, float g) {
+	super(f, g);
+  }
+
+  /**
+   * FIX THIS NONSENSE AS WELL!
+   */
+  private static final long serialVersionUID = -8303716750360393247L;
+
+}
