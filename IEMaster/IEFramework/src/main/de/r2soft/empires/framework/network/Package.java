5681d48a11e147d36fd93d74e72e4d7596e42c36
Katharina Fey <kookie@spacekookie.de>
2014-09-25 15:52:45 +0200

    Starting to work on network communication
    Former-commit-id: 335d739b6228b549ca34b51d18ec1ff9659d0451

---
+++ IEMaster/IEFramework/src/main/de/r2soft/empires/framework/network/Package.java
@@ -0,0 +1,38 @@
+/* #########################################################################
+ * Copyright (c) ${year} RANDOM ROBOT SOFTWORKS
+ * (See @authors file)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ######################################################################### */
+
+package de.r2soft.empires.framework.network;
+
+import com.esotericsoftware.jsonbeans.Json;
+import com.esotericsoftware.jsonbeans.Json.Serializable;
+import com.esotericsoftware.jsonbeans.JsonValue;
+
+/**
+ * @author Katharina Fey <kookie@spacekookie.de>
+ */
+public abstract class Package implements Serializable {
+  private float ID;
+
+  @Override
+  public void read(Json arg0, JsonValue arg1) {
+  }
+
+  @Override
+  public void write(Json arg0) {
+  }
+}
