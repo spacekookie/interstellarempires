cebb54a595266645d5311d090a6d1a2ff21fe022
Katharina Fey <kookie@spacekookie.de>
2017-03-20 01:39:19 +0100

    Adding a list of Types for each object that can be referenced as a Trait from anywhere

---
+++ GameFramework/src/io/lonelyrobot/empires/fw/game/traits/Celestial.java
@@ -0,0 +1,11 @@
+package io.lonelyrobot.empires.fw.game.traits;
+
+/**
+ * A trait that marks a celestial body. This is mearly an indicator of orbital dynamics
+ * and has barely any result on gameplay.
+ * 
+ * Move along...
+ * 
+ * @author Katharina 'spacekookie' Fey <kookie@spacekookie.de>
+ */
+public interface Celestial {}
---
+++ GameFramework/src/io/lonelyrobot/empires/fw/game/traits/Types.java
@@ -0,0 +1,28 @@
+package io.lonelyrobot.empires.fw.game.traits;
+
+public class Types {
+
+  public enum Stars {
+
+    /** Run of the mill dwarves */
+    RED_DWARF, YELLOW_DWARF,
+
+    /** Special dwarf stars */
+    BROWN_DWARF, WHITE_DWARF,
+
+    /** Giant sequence (O, B, A, F) */
+    WHITE_GIANT, BLUE_GIANT, BLUE_SUPERGIANT,
+
+    /** Special "stars" */
+    BLACK_HOLE, SUPERNOVA, PULSAR
+  }
+
+  public enum Planetoids {
+    
+  }
+
+  public enum Nebulae {
+
+  }
+
+}
